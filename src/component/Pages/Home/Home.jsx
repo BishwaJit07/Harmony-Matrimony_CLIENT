f6958a12a0c56c12adf83a119030a20545c925d7
BishwaJit07 <bishwajitr50@gmail.com>
2023-08-08 14:06:11 +0600

    homepage text

--- src/component/Pages/Home/Home.jsx
+++ src/component/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='text-cener'>
+        <div className='text-cener text-3xl'>
            Home 
         </div>
     );
