2431731feff8a75b8197c411e5363cde958674aa
BishwaJit07 <bishwajitr50@gmail.com>
2023-08-09 15:11:06 +0600

    logo

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -3,9 +3,11 @@ const Footer = () => {
   return (
     <div>
       {/* THIS IS DEMO FOOTER  */}
-      <footer className="footer p-10 red-primary text-white rounded-t-2xl">
-
-        <div className="lg:ps-8 lg:p-0">
+      <footer className="footer p-10 red-primary text-white rounded-t-2xl flex flex-col md:flex-row justify-center items-center">
+             <div>
+              <img className="w-60 md:w-40" src="https://i.ibb.co/mqcyHYS/20230809-145351.png" alt="" />
+             </div>
+        <div className="lg:ps-8 lg:p-0 ">
           <span className=" uppercase text-3xl font-extralight text-white pb-4">Get In Touch</span>
           <p>Address : Lane City Jackson, USA</p>
           <p>Phone : +92 (8800) 435434</p>
