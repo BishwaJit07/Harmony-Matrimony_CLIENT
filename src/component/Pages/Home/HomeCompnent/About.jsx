09eba4ba43275fea9af0f7e16fe76291ceaaa2d3
BishwaJit07 <bishwajitr50@gmail.com>
2023-08-09 13:09:46 +0600

    resposive about

--- src/component/Pages/Home/HomeCompnent/About.jsx
+++ src/component/Pages/Home/HomeCompnent/About.jsx
@@ -3,15 +3,13 @@ import React from "react";
 const About = () => {
   return (
     <div className="  m-2">
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-4">
-        <div className="text-red-600">
-          <h2 className="text-xl">About <span className=" font-serif decoration-red-700 text-3xl">"SoulMate Matrimony"</span></h2>
-        </div>
-        <div className=" font-mono">Welcome to Soulmate Matrimony, the ultimate destination for individuals seeking a lifelong partnership built on trust, compatibility, and love.</div>
-        <div><img className="rounded" src="https://i.ibb.co/yNXcGc3/Wedding-at-OMG-Villa-Bali.jpg" alt="" /></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+        <div className=" font-mono  align-middle">Welcome to Soulmate Matrimony, the ultimate destination for individuals seeking a lifelong partnership built on trust, compatibility, and love.</div>
+        <div><img className="rounded-xl" src="https://i.ibb.co/yNXcGc3/Wedding-at-OMG-Villa-Bali.jpg" alt="" /></div>
 
+        
+        <div> <img className="rounded-xl" src="https://i.ibb.co/h18P50G/01197-619-M-1-1024x683.jpg" alt="" /></div>
         <div></div>
-        <div> <img className="rounded" src="https://i.ibb.co/h18P50G/01197-619-M-1-1024x683.jpg" alt="" /></div>
       </div>
       <div></div>
     </div>
