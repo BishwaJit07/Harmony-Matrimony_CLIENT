6524e2581d882f0827f5dbaa1824262deb21fec4
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-08-13 21:02:36 +0600

    create about route

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -4,6 +4,7 @@ import Home from "../component/Pages/Home/Home";
 import Main from "../Layouts/Main";
 import SignIn from "../component/Pages/SignIn/SignIn";
 import SignUp from "../component/Pages/SignUp/SignUp";
+import About from "../component/Pages/About/About";
 
 const router = createBrowserRouter([
   {
@@ -22,6 +23,10 @@ const router = createBrowserRouter([
       {
         path: "/signup",
         element: <SignUp />
+      },
+      {
+        path: "/about",
+        element: <About />
       }
     ],
   },
