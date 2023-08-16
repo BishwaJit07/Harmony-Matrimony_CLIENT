0466999ff18eaf0d6eb7e61ec37b9eb9ef1107b2
naime <naimeh461@gmail.com>
2023-08-16 22:54:48 +0600

    update search all user

--- src/component/Pages/AllUser/AllUser.jsx
+++ src/component/Pages/AllUser/AllUser.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import UserCard from "./UserCard";
 import Loading from "../../../Shared/loading";
 import { useLocation } from "react-router";
+import Search from "./Search";
 
 const AllUser = () => {
         const location = useLocation();
@@ -30,10 +31,13 @@ const AllUser = () => {
     
 
     return (
-        <div className="grid grid-cols-2 w-[80%] mx-auto my-10 gap-10">
+        <div className=" w-[80%] mx-auto">
+          <Search></Search>
+        <div className="grid lg:grid-cols-2 mx-auto my-10 gap-10">
             {
                 users.map((user)=> <UserCard key={user._id} user={user} ></UserCard>)
             }
+        </div>
         </div>
     );
 };
--- src/component/Pages/AllUser/UserCard.jsx
+++ src/component/Pages/AllUser/UserCard.jsx
@@ -1,16 +1,34 @@
-
-const UserCard = ({user}) => {
-    const {img,name}  = user
+import { BsChatSquareHeartFill , BsFillEnvelopeFill, BsFillTelephoneFill } from "react-icons/bs";
+const UserCard = ({ user }) => {
+    const { img, name, country, religion, physical_attributes, phone, email, gender } = user
     return (
         <div>
-            <div className="flex p-4 rounded-lg bg-base-100 shadow-xl bg items-center gap-10">
-                <figure><img  src={img} alt="Movie" /></figure>
-                <div className="">
-                    <h2 className="card-title">{name}</h2>
-                    <p>Click the button to watch on Jetflix app.</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Watch</button>
+            <div className="flex  p-3 lg:p-5 rounded-lg shadow-xl bg gap-5 bg-[#effff8]">
+                <figure className="w-[100%] md:w-[50%] lg:w-[30%] hover:scale-105 duration-300 "><img src={img} alt="Movie" /></figure>
+                <div className="lg:w-[100%] w-[70%] md:w-[50%]  flex flex-col flex-grow-0 ">
+                <div >
+                    <h2 className="text-2xl font-bold mb-5">{name}</h2>
+                </div>
+                <div className="flex flex-wrap gap-1 lg:gap-4">
+                    <h2 className="text-white bg-[#99dabc] py-1 px-2 rounded text-xs">{gender}</h2>
+                    <h2 className="text-white bg-[#99dabc] py-1 px-2 rounded text-xs">{country}</h2>
+                    <h2 className="text-white bg-[#99dabc] py-1 px-2 rounded text-xs">{religion}</h2>
+                    <h2 className="text-white bg-[#99dabc] py-1 px-2 rounded text-xs">{physical_attributes?.height}</h2>
+                    <h2 className="text-white bg-[#99dabc] py-1 px-2 rounded text-xs">{physical_attributes?.weight}</h2>
+                </div>
+                <hr className="h-px my-6 bg-gray-200 border-0 dark:bg-gray-400 mt-10 xl:mt-auto" />
+                <div className="flex flex-grow-0 ">
+                    <div className="flex gap-1">
+                        <button className=" hover:bg-[#FF725E] hover:text-white hover:rounded-full hover:duration-300 py-2 px-2 text-base"><BsChatSquareHeartFill/></button>
+                        <button className=" hover:bg-[#FF725E] hover:text-white hover:rounded-full hover:duration-300 py-2 px-2 text-lg"><BsFillEnvelopeFill/></button>
+                        <button className=" hover:bg-[#FF725E] hover:text-white hover:rounded-full hover:duration-300 py-2 px-2 text-lg"><BsFillTelephoneFill/></button>
+                       
                     </div>
+                    <button className="text-white bg-[#FF725E] py-1 px-2 rounded text-xs  ml-auto">More Details</button>
+                <div>
+                  
+                </div>
+                </div>
                 </div>
             </div>
         </div>
