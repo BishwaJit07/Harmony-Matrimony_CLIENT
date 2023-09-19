67f83191b087896cd8e017917fe1cff64a2af4a8
Bishwajit07 <bishwajitr50@gmail.com>
2023-09-19 23:40:07 +0600

    link udated

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -6,7 +6,7 @@ import useAuth from './useAuth';
 
 
 const axiosSecure = axios.create({
-  baseURL: 'soulmate-server-routed.vercel.app/', 
+  baseURL: 'https://soulmate-server-routed.vercel.app/', 
 });
 
 const useAxiosSecure = () => {
