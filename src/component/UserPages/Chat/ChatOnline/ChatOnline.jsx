d73e83369268d75a1417ef229107525265adad49
Bishwajit07 <bishwajitr50@gmail.com>
2023-09-21 18:19:24 +0600

    dash name

--- src/component/UserPages/Chat/ChatOnline/ChatOnline.jsx
+++ src/component/UserPages/Chat/ChatOnline/ChatOnline.jsx
@@ -12,7 +12,7 @@ const ChatOnline = ({ onlineUsers, currentId, setCurrentChat , refetch}) => {
 
     useEffect(() => {
         const getUsers = async () => {
-            const res = await axios.get("https://soulmate-server-routed.vercel.app/allUser");
+            const res = await axios.get("https://soulmates-server.vercel.app/allUser");
             setAllUser(res.data);
         };
 
@@ -26,7 +26,7 @@ const ChatOnline = ({ onlineUsers, currentId, setCurrentChat , refetch}) => {
     const handleClick = async (user) => 
     {
         try{
-            const res =await axios.get(`https://soulmate-server-routed.vercel.app/conversations/find/${currentId}/${user._id}`)
+            const res =await axios.get(`https://soulmates-server.vercel.app/conversations/find/${currentId}/${user._id}`)
             setCurrentChat(res.data)
             refetch()
         }
