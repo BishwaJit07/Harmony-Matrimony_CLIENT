aef4ad091adbe24f81e9f105be9e374fbd27974d
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-09-23 11:26:19 +0600

    added gallery photo in profile page

--- src/component/AuthPages/Dashboard/Shared/DashboardProfile.jsx
+++ src/component/AuthPages/Dashboard/Shared/DashboardProfile.jsx
@@ -16,24 +16,15 @@ const DashboardProfile = () => {
   },[user])
 
     return (
-        <div>
-           {/* {
-            profileDatas?.map(data => div)
-           } */}
-             <div className="ms-96 ps-52 avatar my-5">
-              <div className="w-60 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                  <img className="h-40 w-40" src={profileDatas?.profileImage}/>
-                </div>
-            </div>
-              <div >
-                <h4 className="text-center text-3xl my-2 text-cyan-700 font-medium">{profileDatas?.name}</h4>
-               <p className="text-center text-xl my-2 text-cyan-700 font-medium">{profileDatas?.email}</p>
-               <div className='ms-56 my-3'>
-               <h6 className="text-center text-lg text-gray-500 border-2 border-gray-500 w-1/3 ms-72 p-3  font-medium italic">{profileDatas?.role} || {profileDatas?.status}</h6>
-               </div>
-              </div>
-           
+      <div className='h-screen'>
+        <div className="flex flex-col items-center ">
+          <img className="w-64 h-64 object-cover object-top rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 mt-28" src={profileDatas?.profileImage} alt="" />
+          <h4 className="text-center text-3xl text-cyan-700 font-medium mt-8">{profileDatas?.name}</h4>
+          <p className="text-center text-xl my-2 text-cyan-700 font-medium">{profileDatas?.email}</p>
+          <h6 className="text-center text-lg text-gray-500 border-2 border-gray-500 w-1/3 p-3  font-medium italic">{profileDatas?.role} || {profileDatas?.status}</h6>
         </div>
+           
+      </div>
     );
 };
 
